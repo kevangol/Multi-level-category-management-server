c019f195ef1a441e2a1d149b20f40b1cc10d677b
kevangol <kevankumar431@gmail.com>
2025-02-16 19:42:25 +0530

    cors changes

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.use(mongoSanitize()); // Prevent NoSQL Injection
 
 app.use(
     cors({
-        origin: "http://localhost:5173",
+        origin: "https://multi-level-category.vercel.app",
         credentials: true,
         methods: ["GET", "POST", "PUT", "DELETE"],
         allowedHeaders: ["Content-Type", "Authorization"],
